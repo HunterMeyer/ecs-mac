dc26138c45097fe2e9cc2c7815f7f36339fab525
Hunter Meyer <huntermeyer@live.com>
2013-11-01 16:03:51 -0700

    Add sign_up form for attendees and vendors

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Ecs::Application.routes.draw do
   devise_for :vendors
   devise_for :attendees
   root 'static_pages#index'
-  match '/faq',      to: 'static_pages#faq',      via: 'get'
-  match '/lectures', to: 'static_pages#lectures', via: 'get'
+
+  match '/faq',       to: 'static_pages#faq',      via: 'get'
+  match '/lectures',  to: 'static_pages#lectures', via: 'get'
+  match '/attendees', to: 'attendees#index',       via: 'get'
+  match '/vendors',   to: 'vendors#index',         via: 'get'
 end
