7b6de3647e7852720549854c61c3ba64e7f72a77
Hunter Meyer <huntermeyer@live.com>
2013-11-01 20:04:13 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Ecs::Application.routes.draw do
   devise_for :admins
   resources  :admins,    only: [:index, :show]
-  resources  :attendees
-  resources  :vendors
+  resources  :attendees, only: [:create, :new]
+  resources  :vendors,   only: [:create, :new]
   
   root 'static_pages#index'
 
   match '/faq',       to: 'static_pages#faq',      via: 'get'
   match '/lectures',  to: 'static_pages#lectures', via: 'get'
-
 end
