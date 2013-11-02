3af004bbd9fc15c3799b61e3a9ed9c5cd153deab
Hunter Meyer <huntermeyer@live.com>
2013-11-01 18:06:53 -0700

    CRUD attendees, vendors and admins

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -2,4 +2,9 @@ class AttendeesController < ApplicationController
   def index
   	@attendees = Attendee.all
   end
+
+  def show
+    @attendee = Attendee.find(params[:id])
+  end  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Ecs::Application.routes.draw do
   devise_for :admins
   devise_for :vendors
   devise_for :attendees
+
+  resources  :admins,    only: [:index, :show]
+  resources  :attendees, only: [:index, :show]
+  resources  :vendors,   only: [:index, :show]
+  
   root 'static_pages#index'
 
   match '/faq',       to: 'static_pages#faq',      via: 'get'
   match '/lectures',  to: 'static_pages#lectures', via: 'get'
-  match '/attendees', to: 'attendees#index',       via: 'get'
-  match '/vendors',   to: 'vendors#index',         via: 'get'
+
 end
