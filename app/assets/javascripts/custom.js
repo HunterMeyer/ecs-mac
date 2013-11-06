9efae2bd7dc5147d4c42760d9364e7c70c2c5224
Hunter Meyer <huntermeyer@live.com>
2013-11-05 21:43:32 -0800

    Align font color

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,12 @@
-$(function() {
+$(document).on('page:load', function() {
+  $('.button-link').hover(function() {
+    $(this).children().removeClass('icon-white');
+  }, function() {
+    $(this).children().addClass('icon-white');
+  });
+});
+
+$(document).ready(function() {
   $('.button-link').hover(function() {
     $(this).children().removeClass('icon-white');
   }, function() {
