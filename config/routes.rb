7320bd634b76d7acf2cdffe9e2debfb2b7770826
Hunter <Hunter@Hunters-Mac.local>
2013-11-16 17:14:13 -0800

    Update payment_notifications routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Ecs::Application.routes.draw do
-  resources  :payment_notifications
+  resources  :payment_notifications, only: [:create]
   devise_for :admins
   resources  :admins
   resources  :attendees
   resources  :vendors
 
   root 'static_pages#index'
-
-  match '/faq',       to: 'static_pages#faq',      via: 'get'
-  match '/lectures',  to: 'static_pages#lectures', via: 'get'
+  match '/payment_notifications', to: 'payment_notifications#create', via: 'post'
+  match '/faq',       to: 'static_pages#faq',             via: 'get'
+  match '/lectures',  to: 'static_pages#lectures',        via: 'get'
 end
