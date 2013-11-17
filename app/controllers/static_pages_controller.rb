c57fbb3acd1baec2277125733e535f7d4c3a10b0
Hunter <Hunter@Hunters-Mac.local>
2013-11-16 20:16:00 -0800

    Move lectures controller to static_pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,23 @@
 class StaticPagesController < ApplicationController
-  def index
-  end
+  def lectures
+		@lecture = Lecture.new
+	end
+
+	def create
+		@lecture = Lecture.new(lecture_params)
+		if @lecture.save
+			flash[:success] = 'Thanks for the input.'
+			redirect_to lectures_path
+		else
+			render 'lectures'
+	end
 
   def faq
   end
 
-  def lectures
-  end
+  private
+
+	def lectures_params
+		params.require(:lectures).permit(:topic, :email)
+	end
 end
