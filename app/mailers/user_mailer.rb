7bec14c73897dc2eddcdedecaef8a937e644b354
Hunter Meyer <huntermeyer@live.com>
2013-11-18 10:43:03 -0800

    Finish user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,9 +3,10 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
+    email_with_name = "#{@user.first_name} #{@user.last_name} <#{@user.email}>"
     @url  = 'http://rails-ecs.herokuapp.com'
     mail(
-      to: @user.email,
+      to: email_with_name,
       subject: 'Welcome to Engineered Corrusion Solutions 2014'
 
       )
