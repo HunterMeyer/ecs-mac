b3125377acf01749342d9e64efdc80e36db3d94c
Hunter Meyer <huntermeyer@live.com>
2013-11-18 11:49:26 -0800

    Remove Gmail account

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'hunnafresh@gmail.com',
-  password:             'triple666',
+  user_name:            #email_address,
+  password:             #password,
   authentication:       :login,
   enable_starttls_auto: true  
 }
