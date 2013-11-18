f5a047f47b1af4510334121ad702e5f89b381cac
Hunter Meyer <huntermeyer@live.com>
2013-11-18 10:32:23 -0800

    Create user_mailer model

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,12 @@
+class UserMailer < ActionMailer::Base
+  default from: 'from@example.com'
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(
+      to: @user.email
+      
+      )
+  end
+end
