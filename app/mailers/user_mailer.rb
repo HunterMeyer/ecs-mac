d026067f3fef93a1aed9d5da7c3bb2a48cdf3353
Hunter Meyer <huntermeyer@live.com>
2013-11-18 12:29:01 -0800

    CHange default from address

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'ECS <no-reply@engineeredcorrusionsolutions.com>'
 
   def welcome_email(user)
     @user = user
