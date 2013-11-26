887d66c868516d778d6cf098d2a53a078207a0a9
Mac Eisenberg <maceisen@gmail.com>
2013-11-25 17:29:52 -0800

    add google analytics

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     email_with_name = "#{@user.first_name} #{@user.last_name} <#{@user.email}>"
-    @url  = 'http://rails-ecs.herokuapp.com'
+    @url  = 'http://ecs-mac.herokuapp.com'
     mail(
       to: email_with_name,
       subject: 'Welcome to Engineered Corrusion Solutions 2014'
