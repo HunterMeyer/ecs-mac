8619695ba7309d168701ced1c19d2ab4b5fe0fe5
Hunter Meyer <huntermeyer@live.com>
2013-11-27 10:20:17 -0800

    Remove domain from email

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,6 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.mandrillapp.com',
   port:                 587,
-  domain:               'gmail.com',
   user_name:            'huntermeyer@live.com',
   password:             'ER3kQ5R01FxK0hgE4X3hFw',
   authentication:       :login,
