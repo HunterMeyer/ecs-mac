ee73fde0ae8225f262c5af03f70110600dbf4b5f
Hunter Meyer <huntermeyer@live.com>
2013-11-27 11:13:11 -0800

    Send email after attendee pays

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -3,11 +3,6 @@ class PaymentNotificationsController < ApplicationController
 
   def create
   	PaymentNotification.create!(:params => params, :attendee_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
-  	if params[:payment_status] == 'Complete'
-      @attendee = Attendee.find(params[:invoice])
-  	  UserMailer.welcome_email(@attendee).deliver
-      @attendee.update_attribute(:paid, Time.now)
-  	end
-    render nothing: true
+  	render nothing: true
   end
 end
