4292d198edc90244230b57f8c5cb7b8d90c6dbe6
Hunter Meyer <huntermeyer@live.com>
2013-11-27 08:55:15 -0800

    Make attendee instance var in IPN

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -4,8 +4,8 @@ class PaymentNotificationsController < ApplicationController
   def create
   	PaymentNotification.create!(:params => params, :attendee_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
   	if params[:payment_status] == 'Complete'
-      attendee = Attendee.find(params[:invoice])
-  	  attendee.update_attribute(:paid, Time.now)
+      @attendee = Attendee.find(params[:invoice])
+  	  @attendee.update_attribute(:paid, Time.now)
   	  UserMailer.welcome_email(@attendee).deliver
   	end
     render nothing: true
